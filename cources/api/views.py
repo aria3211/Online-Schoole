eaf7e38d3760d39df7acacd307fcb89c9469f337
aria3211 <aryapoint1@gmail.com>
2025-01-18 01:40:01 +0330

    update api Subject

--- cources/api/views.py
+++ cources/api/views.py
@@ -2,11 +2,14 @@ from rest_framework import generics
 from django.db.models import Count
 from cources.models import Subject
 from cources.api.serializers import SubjectSerializer
+from cources.api.pagination import StandartPagination
 
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandartPagination
+        
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
--- educa/urls.py
+++ educa/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
@@ -31,6 +32,7 @@ urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api/', include('cources.api.urls', namespace='api')),
 
 ]
 
